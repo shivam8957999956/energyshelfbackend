60da888c9f7724ee93c40349dd56239adf0746fe
shivam <084ssharan@gmail.com>
2023-03-27 22:23:45 +0530

    first commit

---
+++ models/Post.js
@@ -0,0 +1,72 @@
+const mongoose = require("mongoose");
+
+const PostSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    desc: {
+      type: String,
+      required: true,
+    },
+    photo: {
+      type: String,
+      required: false,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    userId: {
+      type: String,
+      required: true,
+    },
+    categories: {
+      type: Array,
+      required: false,
+    },
+    companies: {
+      type: Array,
+      required: false,
+    },
+    likes: {
+      type: Intl,
+      default: 0,
+    },
+    views: {
+      type: Intl,
+      default: 0,
+    },
+    view: {
+      type: Intl,
+      default: 0,
+    },
+    editor: {
+      type: Boolean,
+      default: false,
+    },
+    question: {
+      type: Boolean,
+      default: false,
+    },
+    quiz: {
+      type: Boolean,
+      default: false,
+    },
+    approved: {
+      type: Boolean,
+      default: false,
+    },
+    bookmark: {
+      type: [String],
+    },
+    liked: {
+      type: [String],
+    },
+  },
+  { timestamps: true },
+);
+
+module.exports = mongoose.model("Post", PostSchema);
